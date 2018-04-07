590c7e84282c8a46a56f9d908ba40b96a5fa5f73
Unknown <clinicalsystemsengineering@gmail.com>
2018-04-06 17:39:04 -0700

    initial commit

---
+++ snpp.go
@@ -0,0 +1,97 @@
+package snpp
+
+import (
+	"bufio"
+	"log"
+	"net"
+	"strings"
+)
+
+//snpp level 1 client
+func Client(msgchan chan string, addr string) {
+
+Init:
+	//dial server
+	snpp, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Printf("Error dialing snpp server: %v\n", err.Error())
+		return
+	}
+	r := bufio.NewReader(snpp)
+
+	//server connection init
+	response, err := r.ReadString('\r')
+	if err != nil {
+		log.Printf("Error reading response from SNPP server: %v\n", err.Error())
+		log.Println("Closing connection and starting new connection.")
+		snpp.Close()
+		goto Init
+	}
+	if !strings.Contains(response, "220") {
+		log.Printf("SNPP Server was not ready. response was: %v\n", response)
+		snpp.Close()
+		goto Init
+	}
+
+	//start sending messages
+	for {
+
+		//parse message from channel
+		msg := <-msgchan
+		splitmsg := strings.Split(msg, ";")
+		pin, text := splitmsg[0], splitmsg[1]
+
+		//initiate page
+		snpp.Write([]byte("PAGE " + pin + "\r"))
+
+		//read response from page initiate
+		response, err = r.ReadString('\r')
+		if err != nil {
+			log.Printf("Error reading response from SNPP server: %v\n", err.Error())
+			log.Println("Closing connection and starting new connection.")
+			snpp.Close()
+			msgchan <- msg
+			goto Init
+		}
+		//if page not accepted move on and throw out the message
+		if !strings.Contains(response, "250") {
+			log.Printf("SNPP Server did not accept pager id. response was: %v\n", response)
+			log.Printf("THROWING OUT MSG: %v\n", msg)
+		} else {
+			snpp.Write([]byte("MESS " + text + "\r"))
+			response, err = r.ReadString('\r')
+			if err != nil {
+				log.Printf("Error reading response from SNPP server: %v\n", err.Error())
+				log.Println("Closing connection and starting new connection.")
+				snpp.Close()
+				msgchan <- msg
+				log.Printf("REQUEUED MSG: %v\n", msg)
+				goto Init
+			}
+
+			if !strings.Contains(response, "250") {
+				log.Printf("SNPP Server did not accept message. response was: %v\n", response)
+				msgchan <- msg
+				log.Printf("REQUEUED MSG: %v\n", msg)
+			} else {
+				snpp.Write([]byte("SEND\r"))
+				response, err = r.ReadString('\r')
+				if err != nil {
+					log.Printf("Error reading response from SNPP server: %v\n", err.Error())
+					log.Println("Closing connection and starting new connection.")
+					snpp.Close()
+					msgchan <- msg
+					log.Printf("REQUEUED MSG: %v\n", msg)
+					goto Init
+				}
+				if !strings.Contains(response, "250") {
+					log.Printf("SNPP Server did not send message. response was: %v\n", response)
+					msgchan <- msg
+					log.Printf("REQUEUED MSG: %v\n", msg)
+
+				}
+			}
+
+		}
+	}
+}
